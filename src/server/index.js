beae1c79be3839520b31eeccd6273e3c02af8b22
Julio Torres <jtorres990@gmail.com>
2023-01-31 16:52:43 -0500

    feat(error): se agrega controlador de errores

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const createHttpError = require('http-errors')
 const applyEndpoints = require('./endpoints')
 const applyMiddlewares = require('./middlewares')
 
@@ -11,6 +12,30 @@ const createExpressServer = async (app) => {
   const server = express()
   applyMiddlewares(server, app)
   applyEndpoints(server, app)
+
+  server.use(
+    /**
+     * @param {import('http-errors').CreateHttpError | Error} error
+     * @param {import('express').Request} req
+     * @param {import('express').Response<Log[]>} res
+     * @param {import('express').NextFunction} next
+     */
+    function (error, req, res, next) {
+      // implementar registros de errores?
+
+      if (createHttpError.isHttpError(error)) {
+        res.status(error.statusCode)
+        res.json({
+          message: error.message,
+        })
+        return
+      }
+      res.status(500)
+      res.json({
+        message: error.message,
+      })
+    }
+  )
 
   await app.db.initDB()
 
